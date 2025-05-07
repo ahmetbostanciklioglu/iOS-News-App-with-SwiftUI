c053d884fa6175fe7fa47d34818c1caf77c66d9a
ahmetbostanciklioglu <ahmetbostancikli@gmail.com>
2025-05-07 22:51:02 +0300

    Write your api key here

--- iOS News App with SwiftUI/Constants/Constants.swift
+++ iOS News App with SwiftUI/Constants/Constants.swift
@@ -9,5 +9,5 @@ import Foundation
 
 enum Constants {
     static var baseURL = "https://newsapi.org/v2/"
-    static var apiKey = "47dcd6a172a5403f8de0ba3075497db8"
+    static var apiKey = "Write-your-api-key-here"
 }
